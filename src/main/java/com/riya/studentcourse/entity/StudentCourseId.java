28a691769779d6cc96edb64cb1962c3334e6624e
riya.mishra <riya.mishra@RTC-DLLA39.rtctek.global>
2025-06-11 11:09:09 +0530

    Modified on 11-06-2025

--- src/main/java/com/riya/studentcourse/entity/StudentCourseId.java
+++ src/main/java/com/riya/studentcourse/entity/StudentCourseId.java
@@ -34,10 +34,10 @@ public class StudentCourseId implements Serializable {
 	 }
 	
 	 @Override
-	 public boolean equals(Object o) {
-	     if (this == o) return true;
-	     if (o == null || getClass() != o.getClass()) return false;
-	     StudentCourseId that = (StudentCourseId) o;
+	 public boolean equals(Object object) {
+	     if (this == object) return true;
+	     if (object == null || getClass() != object.getClass()) return false;
+	     StudentCourseId that = (StudentCourseId) object;
 	     return Objects.equals(studentId, that.studentId) &&
 	            Objects.equals(courseId, that.courseId);
 	 }
